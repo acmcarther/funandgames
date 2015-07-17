8e83827d37dfdf621c87ed8bf12fbd691f5418c3
Alex McArther <acmcarther@gmail.com>
2015-07-17 08:58:02 -0700

    server woot

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ use std::ascii::AsciiExt;
 use std::io::stdin;
 use std::net::{SocketAddr, UdpSocket};
 use std::io::Error;
+use std::sync::mpsc::channel;
+use std::thread;
+use std::collections::HashMap;
 
 #[derive(Debug)]
 enum NetMode {
@@ -23,11 +26,7 @@ struct ServerParams {
 }
 
 struct ServerState {
-  users: Vec<User>
-}
-
-struct User {
-  connection: SocketAddr
+  users: HashMap<String, SocketAddr>
 }
 
 const CODE_WORD: &'static str = "funandgames";
@@ -73,51 +72,95 @@ fn net_mode_from_string(mode_str: &str) -> NetMode {
 
 fn be_server(socket: UdpSocket) {
   println!("now hosting!");
-  let mut server_state = ServerState { users: Vec::new() };
+  let mut server_state = ServerState { users: HashMap::new() };
   loop {
     let mut buf = [0; 256];
     socket.recv_from(&mut buf)
       .map_err(log_error)
       .map(|(_, socket_addr)| (socket_addr, stringify_bytes(&buf)))
       .map(|(socket_addr, msg)| filter_msg_for_relevance(socket_addr, msg))
-      .map(|possible_payload| reply(&socket, possible_payload));
+      .map(|possible_payload| add_user(&mut server_state, possible_payload))
+      .map(|possible_payload| reply_all(&server_state, &socket, possible_payload));
   }
 }
 
 fn be_client(socket: UdpSocket, client_params: ClientParams) {
-  loop {
+  let (stdin_tx, stdin_rx) = channel();
+  let (send_tx, send_rx) = channel();
+  let recv_socket = socket.try_clone().unwrap();
+
+  // Console IO
+  thread::spawn (move || {
+    let mut stdin = stdin();
+    loop {
+      let mut message = String::new();
+
+      println!("type your message!");
+      stdin.read_line(&mut message);
+      let coded_trimmed_message = CODE_WORD.to_string() + message.trim();
+      stdin_tx.send(coded_trimmed_message).unwrap();
+    }
+  });
+
+  // Sending messages
+  thread::spawn (move || {
     let server_addr = client_params.server_addr.to_string();
     let server_port = client_params.server_port.to_string();
     let full_server_addr_string: String = server_addr + ":" + &server_port;
     let full_server_addr: &str = full_server_addr_string.as_ref();
-    let mut stdin = stdin();
-    let mut message = String::new();
 
-    println!("type your message!");
-    stdin.read_line(&mut message);
-    let coded_trimmed_message = CODE_WORD.to_string() + message.trim();
+    loop {
+      let message_to_deliver: String = send_rx.recv().unwrap();
+      socket.send_to(message_to_deliver.as_bytes(), full_server_addr)
+        .map_err(log_error);
+    }
+  });
 
-    println!("now sending [{}] to {}", coded_trimmed_message, full_server_addr);
-    socket.send_to(coded_trimmed_message.as_bytes(), full_server_addr)
-      .map_err(log_error)
-      .map( |_| {
-        let mut buf = [0; 256];
-        println!("now waiting for response");
-        socket.recv_from(&mut buf)
-          .map(|(_, socket_addr)| (socket_addr, stringify_bytes(&buf)))
-          .map(|(socket_addr, msg)| filter_msg_for_relevance(socket_addr, msg))
-          .map(log_response);
-      });
+  // Receiving messages
+  thread::spawn (move || {
+    loop {
+      let mut buf = [0; 256];
+      recv_socket.recv_from(&mut buf)
+        .map(|(_, socket_addr)| (socket_addr, stringify_bytes(&buf)))
+        .map(|(socket_addr, msg)| filter_msg_for_relevance(socket_addr, msg))
+        .map(log_response);
+    }
+  });
+
+  // The bucket brigade
+  loop {
+    let res = stdin_rx.recv().unwrap();
+    send_tx.send(res).unwrap();
   }
 }
 
-fn reply(socket: &UdpSocket, possible_payload: Option<(SocketAddr, String)>) {
+fn add_user(server_state: &mut ServerState, possible_payload: Option<(SocketAddr, String)>) -> Option<String> {
+  println!("add user");
   possible_payload.map(|(socket_addr, msg)| {
-    println!("received message [{}] from {:?}", msg, socket_addr);
-    let return_msg = deduce_return_msg(msg);
-    println!("now sending [{}] to {}", return_msg, socket_addr);
-    socket.send_to(return_msg.as_bytes(), socket_addr)
+    let key = socket_addr.to_string();
+    if !server_state.users.contains_key(&key) {
+      println!("saving user");
+      server_state.users.insert(key, socket_addr.clone());
+    }
+    socket_addr.to_string() + ": " + &msg
+  })
+}
+
+fn reply_all(server_state: &ServerState, socket: &UdpSocket, possible_payload: Option<String>) {
+  possible_payload.map(|msg| {
+    println!("{:?}", server_state.users);
+    let lotsOfNothing: Vec<()> = server_state.users.values().map (|socketAddr| {
+      println!("replying");
+      reply(socket, socketAddr, &msg)
+    }).collect();
   });
+}
+
+fn reply(socket: &UdpSocket, socket_addr: &SocketAddr, msg: &String, ) {
+  println!("received message [{}] from {:?}", msg, socket_addr);
+  let return_msg = deduce_return_msg(&msg);
+  println!("now sending [{}] to {}", return_msg, socket_addr);
+  socket.send_to(return_msg.as_bytes(), socket_addr);
 }
 
 fn stringify_bytes(bytes: &[u8]) -> String {
@@ -134,13 +177,9 @@ fn filter_msg_for_relevance(socket_addr: SocketAddr, msg: String) -> Option<(Soc
   }
 }
 
-fn deduce_return_msg(msg: String) -> String {
+fn deduce_return_msg(msg: &String) -> String {
   let code_word_string = CODE_WORD.to_string();
-  if (&msg == "hello") {
-    code_word_string + "goodbye"
-  } else {
-    code_word_string + "what u say?"
-  }
+  code_word_string + msg
 }
 
 
